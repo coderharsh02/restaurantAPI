c7c2704b38ee65719614e6a0e08be902de6f5e65
Harsh Shah <harshshah.coder@gmail.com>
2022-12-17 23:37:14 +0530

    REST API and mongoDB database project files

---
+++ README.md
@@ -0,0 +1,85 @@
+# RestaurantAPI CRUD Operation 
+
+## Libraries and Frameworks
+- NodeJS
+- ExpressJS
+- Mongoose
+- MongoDB Atlas
+- cors (cross origin resource sharing)
+
+## REST API (/api/restaurants)
+
+### GET Requests (Retrieve)
+- /api/restaurants?page=1&perPage=10 borough=Queens
+
+- /api/restaurants/:id
+
+
+### POST Requests (Create)
+- /api/restaurants/  
+
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Body
+
+```
+{
+    "address": {
+        "building": "97-22",
+        "coord": [
+            -73.8601152,
+            40.7311739
+        ],
+        "street": "63 Road Loud",
+        "zipcode": "11374"
+    },
+    "borough": "Queens",
+    "cuisine": "Jewish/Kosher",
+    "grades": [
+        {
+        "date": "2014-11-24T00:00:00.000Z",
+        "grade": "Z",
+        "score": 20
+        }
+    ],
+    "name": "My New Restaurant",
+    "restaurant_id": "123456"
+}
+```
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Response object along with id such as: 639e00a43c8e09cedecf134c
+
+
+### PUT Requests (Update)
+- /api/restaurants/:id  
+- /api/restaurants/639e00a43c8e09cedecf134c
+
+```
+{
+    "address": {
+        "building": "97-22",
+        "coord": [
+            -73.8601152,
+            40.7311739
+        ],
+        "street": "63 Road Loud",
+        "zipcode": "11374"
+    },
+    "borough": "Queens",
+    "cuisine": "Jewish/Kosher",
+    "grades": [
+        {
+        "date": "2014-11-24T00:00:00.000Z",
+        "grade": "Z",
+        "score": 20
+        }
+    ],
+    "name": "My New Restaurant Updated",
+    "restaurant_id": "123456"
+}
+```
+
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Response Message: "restaurantId: 639e00a43c8e09cedecf134c successfully updated"
+
+### Delete Requests (Delete)
+- /api/restaurants/:id
+- /api/restaurants/639e00a43c8e09cedecf134c
+
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Response Message: "restaurantId: 639e00a43c8e09cedecf134c successfully deleted"
---
+++ server.js
@@ -0,0 +1,114 @@
+// ################################################################################
+//  REST API based on sample dataset (sample_restaurants) from mongoDB Atlas    ###
+//  CRUD Operations
+// ################################################################################
+
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const RestaurantDB = require("./modules/restaurantDB.js");
+dotenv.config();
+const HTTP_PORT = process.env.PORT || 8080;
+let MONGO_URL = process.env.MONGO_URL;
+const db = new RestaurantDB();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// default route
+app.get("/", (req, res) => {
+  res.json({ message: "API Listening" });
+});
+
+// POST: /api/restaurants
+app.post("/api/restaurants", (req, res) => {
+  db.addNewRestaurant(req.body)
+    .then((newResObj) => res.status(201).json(newResObj))
+    .catch(() => res.status(400).json({ error: "Object Creation Failed" }));
+});
+
+// GET: /api/restaurants
+// EG : url/api/restaurants?page=1&perPage=10&borough=Queens
+app.get("/api/restaurants", (req, res) => {
+  console.log(req.query.page, req.query.perPage, req.query.borough);
+  if (!req.query.page || !req.query.perPage || !req.query.borough) {
+    res.status(400).json({ error: "Parameters MISSING" });
+  } else {
+    if (
+      !isNaN(parseInt(req.query.page)) &&
+      !isNaN(parseInt(req.query.perPage)) &&
+      isNaN(parseInt(req.query.borough))
+    ) {
+      db.getAllRestaurants(req.query.page, req.query.perPage, req.query.borough)
+        .then((resObjs) => res.status(200).json(resObjs))
+        .catch((err) => res.status(500).json(err));
+    } else {
+      res.status(400).json({ error: "Parameters type INVALID" });
+    }
+  }
+});
+
+// GET: /api/restaurants/:id
+// EG : url/api/restaurants/5eb3d668b31de5d588f4292f
+app.get("/api/restaurants/:id", (req, res) => {
+  db.getRestaurantById(req.params.id)
+    .then((resObj) => {
+      if (resObj) {
+        res.status(200).json(resObj);
+      } else {
+        res.status(404).json({ error: "Object not found" });
+      }
+    })
+    .catch((err) => res.status(500).json(err));
+});
+
+// PUT: /api/restaurants/:id
+app.put("/api/restaurants/:id", (req, res) => {
+  db.getRestaurantById(req.params.id)
+    .then((resObj) => {
+      if (resObj) {
+        db.updateRestaurantById(req.body, req.params.id)
+          .then(() =>
+            res
+              .status(201)
+              .json(`restaurantId: ${req.params.id} successfully updated`)
+          )
+          .catch((err) => res.status(500).json(err));
+      } else {
+        res.status(404).json({ error: "Object not found" });
+      }
+    })
+    .catch((err) => res.status(500).json(err));
+});
+
+// DELETE: /api/restaurants/:id
+app.delete("/api/restaurants/:id", (req, res) => {
+  db.deleteRestaurantById(req.params.id)
+    .then((result) => {
+      if (result.deletedCount > 0) {
+        res
+          .status(200)
+          .json(`restaurantId: ${req.params.id} successfully deleted`);
+      } else {
+        res
+          .status(404)
+          .json({ error: `restaurantId: ${req.params.id} doesn't exists` });
+      }
+    })
+    .catch((err) => res.status(500).json(err));
+});
+
+app.use((req, res) => {
+  res.status(404).send("Page Not Found");
+});
+
+db.initialize(MONGO_URL)
+  .then(() => {
+    app.listen(HTTP_PORT, () => {
+      console.log(`server listening on: ${HTTP_PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
